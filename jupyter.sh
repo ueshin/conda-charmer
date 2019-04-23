8c5b58783c204a3e2a680ed33be81d7e37ef59bd
Takuya UESHIN <ueshin@databricks.com>
2019-04-23 13:57:40 +0900

    Add -w option to jupyter.sh as well.

--- jupyter.sh
+++ jupyter.sh
@@ -3,6 +3,16 @@
 _ROOT="$(cd "$(dirname "$0")"; pwd)"
 _BASE_NAME=$(basename "${_ROOT}")
 
+while getopts w: OPT
+do
+    case $OPT in
+        w)  _WORKTREE=$OPTARG
+            ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 source "${_ROOT}"/_env.sh
 
 if [ -z "${CONDA_ENV_PREFIX}" ]; then
@@ -11,6 +21,10 @@ fi
 
 source "${_ROOT}"/_activate.sh
 
-export PYTHONPATH="${_ROOT}"/"${SOURCE_PATH}"
+if [ -z "${_WORKTREE}" ]; then
+    export PYTHONPATH="${_ROOT}"/"${SOURCE_PATH}"
+else
+    export PYTHONPATH="$(cd "${_WORKTREE}"; pwd)"
+fi
 
 jupyter notebook "${_ROOT}"/notebooks
