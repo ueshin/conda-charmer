4a5ab5b21d78ba4071fb2e192d05babaf7e6b75b
Takuya UESHIN <ueshin@databricks.com>
2019-05-11 18:26:02 +0900

    Exit if conda env is not found.

--- _activate.sh
+++ _activate.sh
@@ -4,4 +4,4 @@ export CONDA_ENV_ID=${1:-3.6}
 
 source "${CONDA_HOME}/etc/profile.d/conda.sh"
 
-conda activate "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_${CONDA_ENV_ID}"
+conda activate "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_${CONDA_ENV_ID}" || exit
