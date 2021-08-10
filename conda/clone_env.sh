42f9cde4c982c7692a35647cea661d15ff4a6074
Takuya UESHIN <ueshin@databricks.com>
2021-08-10 13:12:59 -0700

    Fix conda/clone_env.sh

--- conda/clone_env.sh
+++ conda/clone_env.sh
@@ -5,6 +5,6 @@ _BASE_NAME=$(basename "${_ROOT}")
 
 source "${_ROOT}"/_env.sh
 
-source "${CONDA_HOME}/etc/profile.d/conda.sh"
+source "${CONDA_PROFILE}"
 
 conda create --prefix "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_$2" --clone "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_$1"
