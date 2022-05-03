5489e961dd21dbc69ebf7a2a4b0fbdccaf32b5fa
Takuya UESHIN <ueshin@happy-camper.st>
2022-05-03 14:30:51 -0700

    `build.sh` should respect the profile.

--- build.sh
+++ build.sh
@@ -13,6 +13,21 @@ else
     export SPARK_HOME="$(cd "${_WORKTREE}"; pwd)"
 fi
 
+if [ -z "${_PROFILE}" ]; then
+    export CONDA_ENV_PROFILE="${CONDA_ENV_ETC}/profile"
+else
+    if [ -f "${CONDA_ENV_ETC}/profile-${_PROFILE}" ]; then
+        export CONDA_ENV_PROFILE="${CONDA_ENV_ETC}/profile-${_PROFILE}"
+    else
+        echo "The profile not found: ${CONDA_ENV_ETC}/profile-${_PROFILE}"
+        exit 1
+    fi
+fi
+
+if [ -f "${CONDA_ENV_PROFILE}" ]; then
+    source "${CONDA_ENV_PROFILE}"
+fi
+
 cd "${SPARK_HOME}" && \
   ./build/sbt clean test:package -Phive-thriftserver && \
   rm python/lib/pyspark.zip
