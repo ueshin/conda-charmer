88b5db58bfa5798825cf7558fdac713b105c9817
Takuya UESHIN <ueshin@happy-camper.st>
2023-04-09 23:04:41 -0700

    Use pip to install libraries instead of conda.

--- conda/create_env.sh
+++ conda/create_env.sh
@@ -13,9 +13,9 @@ source "${CONDA_PROFILE}"
 conda create -y --prefix "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_${CONDA_ENV_ID}" python=${PYTHON_VERSION} && \
 conda activate "${CONDA_ENVS}/${CONDA_ENV_PREFIX}_${CONDA_ENV_ID}" && \
 conda config --add pinned_packages python="${PYTHON_VERSION}" --env && \
-conda install -y -c conda-forge \
+pip install -U \
   jupyter \
   "${ADDITIONAL_PACKAGES[@]}" && \
 if [ -n "${REQUIREMENTS_FILE}" ]; then
-  conda install -y -c conda-forge --file "${_ROOT}/${SOURCE_PATH}/${REQUIREMENTS_FILE}"
+  pip install -U -r "${_ROOT}/${SOURCE_PATH}/${REQUIREMENTS_FILE}"
 fi
